1b622d4c96e57d5af8670b5e6d61498d994bed01
RogerSPPenha <rogerpenha28@gmail.com>
2024-08-15 15:23:06 -0300

    test: Alterado Get

--- front_end/src/paginas/Inicio/index.jsx
+++ front_end/src/paginas/Inicio/index.jsx
@@ -13,8 +13,7 @@ export default function Inicio() {
         <div className={estilos.conteudo}>
           <div className={estilos.banner}>
             <h1 data-test="titulo-principal">
-              Experimente mais liberdade no controle da sua vida financeira.
-              Crie sua conta com a gente!
+              Experimente mais liberdade no controle da sua vida financeira. Crie sua conta com a gente!
             </h1>
             <img
               src={ilustracaoBanner}
