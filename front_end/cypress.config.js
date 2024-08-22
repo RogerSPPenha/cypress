e55e5390efe355500737d5ce5909cf7567243051
RogerSPPenha <rogerpenha28@gmail.com>
2024-08-22 08:40:08 -0300

    :sparkles: feat: adicao teste paginas

--- front_end/cypress.config.js
+++ front_end/cypress.config.js
@@ -2,8 +2,4 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
-});
+  baseUrl: 'http://localhost:3000' }, });
