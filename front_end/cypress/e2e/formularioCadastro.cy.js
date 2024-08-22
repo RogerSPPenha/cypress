1c926729be1faab32640c01b94489635b4f7510d
RogerSPPenha <rogerpenha28@gmail.com>
2024-08-22 11:07:07 -0300

    :sparkles: feat: atualizar testes

---
+++ front_end/cypress/e2e/formularioCadastro.cy.js
@@ -0,0 +1,41 @@
+describe('Formulário cadastro', () => {
+  beforeEach(()=> {
+  cy.visit('http://localhost:3000')
+  })
+  it('Usuário deve conseguir se cadastrar com sucesso', () => {
+  cy.getByData('botao-cadastro').click() 
+  cy.getByData('nome-input').type('Jose da Silva')
+  cy.getByData('email-input').type('ze3@email.com')
+  cy.getByData('senha-input').type('456789')
+  cy.getByData('botao-enviar').click()
+  cy.getByData('mensagem-sucesso').should('exist').and('have.text', 'Usuário cadastrado com sucesso!')
+  })
+  
+  it('Não deve permitir nome em branco', () => {
+    cy.getByData('botao-cadastro').click()
+    
+    cy.getByData('email-input').type('ze3@email.com')
+    cy.getByData('senha-input').type('456789')
+    cy.getByData('botao-enviar').click()
+    cy.getByData('mensagem-erro').should('exist').and('have.text', 'O campo de nome é obrigatório') 
+    
+    }) 
+  it('Não deve permitir email em branco', () => {
+    cy.getByData('botao-cadastro').click()
+    
+    cy.getByData('nome-input').type('Jose da Silva')
+    cy.getByData('senha-input').type('456789')
+    cy.getByData('botao-enviar').click()
+    cy.getByData('mensagem-erro').should('exist').and('have.text', 'E-mail já cadastrado!') 
+    
+    }) 
+  it('Não deve permitir senha em branco', () => {
+    cy.getByData('botao-cadastro').click()
+    
+    cy.getByData('nome-input').type('Jose da Silva')
+    cy.getByData('email-input').type('ze3@email.com')
+    cy.getByData('botao-enviar').click()
+    cy.getByData('mensagem-erro').should('exist').and('have.text', 'E-mail já cadastrado!') 
+    
+    }) 
+})
--- front_end/cypress/e2e/formularioLogin.cy.js
+++ front_end/cypress/e2e/formularioLogin.cy.js
@@ -11,28 +11,22 @@ describe('Formulário de login', () => { // contexto do nosso teste
   cy.getByData('botao-enviar').click()
   cy.getByData('mensagem-erro').should('exist').and('have.text', 'O email digitado é inválido')
   })
-  it.only('Não deve permitir email em branco', () => {
+  it('Não deve permitir email em branco', () => {
     cy.visit('/')
     cy.getByData('botao-login').click()
     
-    // cy.getByData('email-input').type('') /* não passaremos nenhuma informação no campo email, comentando esta linha, pois o comando type não aceita parâmetro vazio */
-    
     cy.getByData('senha-input').type('123456')
     cy.getByData('botao-enviar').click()
-    cy.getByData('mensagem-erro').should('exist').and('have.text', 'O campo email é obrigatório') /* a
-    
-    mensagem exibida deve ser: "o campo email é obrigatório" */
+    cy.getByData('mensagem-erro').should('exist').and('have.text', 'O campo email é obrigatório') 
+  
     }) 
-    it.only('Não deve permitir senha em branco', () => {
+    it('Não deve permitir senha em branco', () => {
       cy.visit('/')
       cy.getByData('botao-login').click()
       
-      // cy.getByData('email-input').type('') /* não passaremos nenhuma informação no campo email, comentando esta linha, pois o comando type não aceita parâmetro vazio */
-      
       cy.getByData('email-input').type('neilton@alura')
       cy.getByData('botao-enviar').click()
-      cy.getByData('mensagem-erro').should('exist').and('have.text', 'O campo de senha é obrigatório') /* a
-      
-      mensagem exibida deve ser: "o campo email é obrigatório" */
+      cy.getByData('mensagem-erro').should('exist').and('have.text', 'O campo de senha é obrigatório')
+    
       }) 
 })
