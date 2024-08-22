0784e9a97381cb9216e541ea1c04b1db995fb3a8
RogerSPPenha <rogerpenha28@gmail.com>
2024-08-22 09:44:39 -0300

    :sparkles: feat: teste do campo senha

--- front_end/cypress/e2e/formularioLogin.cy.js
+++ front_end/cypress/e2e/formularioLogin.cy.js
@@ -11,7 +11,7 @@ describe('Formulário de login', () => { // contexto do nosso teste
   cy.getByData('botao-enviar').click()
   cy.getByData('mensagem-erro').should('exist').and('have.text', 'O email digitado é inválido')
   })
-  it('Não deve permitir email em branco', () => {
+  it.only('Não deve permitir email em branco', () => {
     cy.visit('/')
     cy.getByData('botao-login').click()
     
@@ -23,4 +23,16 @@ describe('Formulário de login', () => { // contexto do nosso teste
     
     mensagem exibida deve ser: "o campo email é obrigatório" */
     }) 
+    it.only('Não deve permitir senha em branco', () => {
+      cy.visit('/')
+      cy.getByData('botao-login').click()
+      
+      // cy.getByData('email-input').type('') /* não passaremos nenhuma informação no campo email, comentando esta linha, pois o comando type não aceita parâmetro vazio */
+      
+      cy.getByData('email-input').type('neilton@alura')
+      cy.getByData('botao-enviar').click()
+      cy.getByData('mensagem-erro').should('exist').and('have.text', 'O campo de senha é obrigatório') /* a
+      
+      mensagem exibida deve ser: "o campo email é obrigatório" */
+      }) 
 })
