506cd7b69170984470b507fba7bb3b29654b57a0
RogerSPPenha <rogerpenha28@gmail.com>
2024-08-23 11:37:29 -0300

    :sparkles: feat: verificar localização no teste paginas

--- front_end/cypress/e2e/paginas.cy.js
+++ front_end/cypress/e2e/paginas.cy.js
@@ -5,9 +5,12 @@ describe('Testando múltiplas páginas', () => {
     cy.getByData('email-input').type('neilton@alura.com')
     cy.getByData('senha-input').type('123456')
     cy.getByData('botao-enviar').click()
+    cy.location('pathname').should('eq','/home')
     cy.getByData('app-home').find('a').eq(1).click()
     cy.getByData('app-home').find('a').eq(0).click()
+    cy.location('pathname').should('eq','/home')
     cy.getByData('titulo-transacao').should('exist').and('have.text','Nova Transação')
+
     })
   it('Deve conseguir acessar a página de cartões', () => {
   cy.visit('/')
@@ -15,8 +18,10 @@ describe('Testando múltiplas páginas', () => {
   cy.getByData('email-input').type('neilton@alura.com')
   cy.getByData('senha-input').type('123456')
   cy.getByData('botao-enviar').click()
+  cy.location('pathname').should('eq','/home')
   cy.getByData('app-home').find('a').eq(1).click()
   cy.getByData('titulo-cartoes').should('exist').and('have.text','Meus cartões')
+  cy.location('pathname').should('eq','/home/cartoes')
   })
   it('Deve conseguir acessar a página serviços', () => {
     cy.visit('/')
@@ -24,8 +29,10 @@ describe('Testando múltiplas páginas', () => {
     cy.getByData('email-input').type('neilton@alura.com')
     cy.getByData('senha-input').type('123456')
     cy.getByData('botao-enviar').click()
+    cy.location('pathname').should('eq','/home')
     cy.getByData('app-home').find('a').eq(2).click()
     cy.get('h5').contains('Empréstimo').should('exist')
+    cy.location('pathname').should('eq','/home/servicos')
     })
     it('Deve conseguir acessar a página investimentos', () => {
       cy.visit('/')
@@ -33,8 +40,10 @@ describe('Testando múltiplas páginas', () => {
       cy.getByData('email-input').type('neilton@alura.com')
       cy.getByData('senha-input').type('123456')
       cy.getByData('botao-enviar').click()
+      cy.location('pathname').should('eq','/home')
       cy.getByData('app-home').find('a').eq(3).click()
       cy.getByData('titulo-investimentos').should('exist').and('have.text','Investimentos')
+      cy.location('pathname').should('eq','/home/investimentos')
       })
 })
 
